881e65b7be40d0c94d97ccfbcc7179d28f5bd9b2
R K Sharath Kumar <sharathk.rk@gmail.com>
2023-07-18 23:46:46 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -43,7 +43,7 @@ def generate_res(text):
    # splitter = CharacterTextSplitter(chunk_size=chunk_size, chunk_overlap=chunk_overlap)
    # chunked_docs = splitter.split_texts(text)
     text_splitter = CharacterTextSplitter(chunk_size=chunk_size, chunk_overlap=chunk_overlap)
-    texts = text_splitter.split_text(txt)
+    texts = text_splitter.split_text(text)
     # Create multiple documents
     docs = [Document(page_content=t) for t in texts]
     # Text summarization
