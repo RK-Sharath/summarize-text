519690e4fb03299199924587d0bdb0f4b06fa04d
R K Sharath Kumar <sharathk.rk@gmail.com>
2023-07-18 23:34:40 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -41,7 +41,7 @@ def generate_res(text):
         ).dict())
     # Split text
     splitter = CharacterTextSplitter(chunk_size=chunk_size, chunk_overlap=chunk_overlap)
-    chunked_docs = splitter.create_documents(text)
+    chunked_docs = splitter.split_documents(text)
     # Text summarization
     chain = load_summarize_chain(llm, chain_type='map_reduce')
     return chain.run(chunked_docs)
