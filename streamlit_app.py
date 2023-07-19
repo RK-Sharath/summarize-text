6a08cec1c10d7707c328af36f05db33f6084a24d
R K Sharath Kumar <sharathk.rk@gmail.com>
2023-07-19 12:08:26 +0530

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -49,7 +49,7 @@ def generate_res(query):
 result = []
 with st.form('summarize_form', clear_on_submit=True):
     submitted = st.form_submit_button('Submit')
-     if submitted and genai_api_key.startswith('pak-'):
+    if submitted and genai_api_key.startswith('pak-'):
           with st.spinner('Working on it...'):
             response = generate_res(input_data)
             result.append(response)
