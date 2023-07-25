0d95e3967c166544dc03cdcdb357fd792b38c88c
R K Sharath Kumar <sharathk.rk@gmail.com>
2023-07-25 22:36:08 +0530

    Update summarize.py

--- summarize.py
+++ summarize.py
@@ -18,7 +18,7 @@ from langchain import PromptTemplate
 st.title("Document Summarization App powered by IBM Watsonx")
 st.caption("This app was developed by Sharath Kumar RK, IBM Ecosystem Engineering Watsonx team")
 
-
+model = st.radio("Select the Watsonx LLM model",('google/flan-t5-xl','google/flan-t5-xxl','google/flan-ul2'))
 genai_api_key = st.sidebar.text_input("GenAI API Key", type="password")
 genai_api_url = st.sidebar.text_input("GenAI API URL", type="default")
 max_new_tokens = st.sidebar.number_input("Select max new tokens")
