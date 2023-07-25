5afe9b6943db1b4d477ee9decfc9b4c050772878
R K Sharath Kumar <sharathk.rk@gmail.com>
2023-07-25 22:50:09 +0530

    Update summarize.py

--- summarize.py
+++ summarize.py
@@ -62,19 +62,25 @@ def setup_documents(chunk_size, chunk_overlap):
 
 
 def custom_summary(docs,llm, custom_prompt, chain_type, num_summaries):
+    
     custom_prompt = custom_prompt + """:\n\n {text}"""
     COMBINE_PROMPT = PromptTemplate(template=custom_prompt, input_variables=["text"])
     MAP_PROMPT = PromptTemplate(template="Summarize:\n\n{text}", input_variables=["text"])
+    
     creds = Credentials(api_key=genai_api_key, api_endpoint=genai_api_url)
+    
     # Define parameters
     params = GenerateParams(decoding_method=decoding_method, temperature=temperature, max_new_tokens=max_tokens, min_new_tokens=min_tokens, repetition_penalty=repetition_penalty)
+    
     # Instantiate LLM model
     llm=LangChainInterface(model=model, params=params, credentials=creds)
+    
     if chain_type == "map_reduce":
         chain = load_summarize_chain(llm, chain_type=chain_type, 
                                     map_prompt=MAP_PROMPT, combine_prompt=COMBINE_PROMPT)
     else:
         chain = load_summarize_chain(llm, chain_type=chain_type)
+        
     summaries = []
     for i in range(num_summaries):
         summary_output = chain({"input_documents": docs}, return_only_outputs=True)["output_text"]
