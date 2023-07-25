7c76a143d6a819cbcab1337a46a4faaeaca4ab2b
R K Sharath Kumar <sharathk.rk@gmail.com>
2023-07-25 22:24:37 +0530

    Update summarize.py

--- summarize.py
+++ summarize.py
@@ -32,9 +32,26 @@ with st.sidebar:
     )
 temperature = st.sidebar.number_input("Temperature (Choose a decimal number between 0 & 2)")
 
+
+uploaded_file = st.file_uploader('', type=(['pdf',"tsv","csv","txt","tab","xlsx","xls"]))
+temp_file_path = os.getcwd()
+while uploaded_file is None:
+    x = 1
+        
+if uploaded_file is not None:
+    # Save the uploaded file to a temporary location
+    temp_dir = tempfile.TemporaryDirectory()
+    temp_file_path = os.path.join(temp_dir.name, uploaded_file.name)
+    with open(temp_file_path, "wb") as temp_file:
+        temp_file.write(uploaded_file.read())
+
+    st.write("Full path of the uploaded file:", temp_file_path)
+
+
+
 @st.cache_data
-def setup_documents(pdf_file_path, chunk_size, chunk_overlap):
-    loader = PyPDFLoader(pdf_file_path)
+def setup_documents(chunk_size, chunk_overlap):
+    loader = PyPDFLoader(temp_file_path)
     docs_raw = loader.load()
     docs_raw_text = [doc.page_content for doc in docs_raw]
     text_splitter = RecursiveCharacterTextSplitter(chunk_size=chunk_size, chunk_overlap=chunk_overlap)
@@ -66,9 +83,8 @@ def custom_summary(docs,llm, custom_prompt, chain_type, num_summaries):
 
 def main():
     user_prompt = st.text_input("Enter the user prompt")
-    pdf_file_path = st.text_input("Enter the pdf file path")
-    if pdf_file_path != "":
-        docs = setup_documents(pdf_file_path, chunk_size, chunk_overlap)
+    if temp_file_path != "":
+        docs = setup_documents(chunk_size, chunk_overlap)
         st.write("Pdf was loaded successfully")
         if st.button("Summarize"):
             result = custom_summary(docs,llm, user_prompt, chain_type, num_summaries)
